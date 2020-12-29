acbcfabe6c2cc802d5b04ad561b2e9685d646161
Simon Reynolds <reynolds.simon4@gmail.com>
2020-12-29 16:15:46 +0100

    Added in UI

---
+++ Styles.js
@@ -0,0 +1,43 @@
+exports.HEADER_STYLE_1 = {
+    fontSize: '32px',
+    fontWeight: 'bold',
+    // padding: '10px',
+    margin: '0 0 4px 8px'
+  };
+
+exports.HEADER_STYLE_2 = {
+  fontSize: '24px',
+  // padding: '0px 10px',
+  margin: '0 0 4px 8px'
+};
+
+exports.TEXT_STYLE = {
+    fontSize: '16px',
+    // padding: '0px 10px',
+    margin: '0 0 4px 8px'
+}
+
+exports.INDICATOR_STYLE = {
+    fontSize: '16px',
+    fontWeight: 'bold',
+    // padding: '0px 10px',
+    margin: '0 0 4px 8px'
+}
+
+exports.INTRO_STYLE = {
+    fontSize: '20px',
+    fontWeight: 'lighter',
+    padding: '20px 0 30px 0',
+    margin: '0 0 4px 8px',
+    color: 'gray'
+}
+
+exports.SECTION_STYLE = {
+  margin: '0 0 10px 0',
+  padding: '0 0 12px 0'
+};
+
+// var CHART_PANEL_STYLE = {
+//   height: '400px',
+//   // margin: '0 0 10px 8px'
+// };
