2d1943e21772f5e40debe032bd09aa2b981eddd5
Simon Reynolds <reynolds.simon4@gmail.com>
2021-01-27 17:10:35 +0100

    parsed the strings to numbers in scenario save

--- ScenarioPlanner.js
+++ ScenarioPlanner.js
@@ -36,13 +36,13 @@ exports.saveScenario = function(regionalData, scenarioLandCoverTransitions, curr
     print(landCoverBaseData)
     print(scenarioLandCoverTransitions)
     print(scenarioLandCoverTransitions['Tree_Cover to Artificial'])
-    var artificial = landCoverBaseData['Artificial'] + scenarioLandCoverTransitions['Tree_Cover to Artificial'] + scenarioLandCoverTransitions['Grasslands to Artificial'] + scenarioLandCoverTransitions['Bare_Land to Artificial'];
-    var bareLand = landCoverBaseData['Bare_land'] - scenarioLandCoverTransitions['Bare_Land to Grasslands'] - scenarioLandCoverTransitions['Bare_Land to Croplands'] - scenarioLandCoverTransitions['Bare_Land to Artificial'];
-    var croplands = landCoverBaseData['Croplands'] + scenarioLandCoverTransitions['Bare_Land to Croplands'] + scenarioLandCoverTransitions['Grasslands to Croplands'] + scenarioLandCoverTransitions['Tree_Cover to Croplands'];
-    var grasslands = landCoverBaseData['Grasslands'] + scenarioLandCoverTransitions['Bare_Land to Grasslands'] + scenarioLandCoverTransitions['Tree_Cover to Grasslands'] - scenarioLandCoverTransitions['Grasslands to Artificial'] - scenarioLandCoverTransitions['Grasslands to Croplands'];
-    var treeCover = landCoverBaseData['Tree_Cover'] - scenarioLandCoverTransitions['Tree_Cover to Artificial'] - scenarioLandCoverTransitions['Tree_Cover to Croplands'] - scenarioLandCoverTransitions['Tree_Cover to Grasslands'];
-    var waterBodies = landCoverBaseData['Water_Bodies'];
-    var wetlands = landCoverBaseData['Wetlands'];
+    var artificial = parseFloat(landCoverBaseData['Artificial']) + parseFloat(scenarioLandCoverTransitions['Tree_Cover to Artificial']) + parseFloat(scenarioLandCoverTransitions['Grasslands to Artificial']) + parseFloat(scenarioLandCoverTransitions['Bare_Land to Artificial']);
+    var bareLand = parseFloat(landCoverBaseData['Bare_land']) - parseFloat(scenarioLandCoverTransitions['Bare_Land to Grasslands']) - parseFloat(scenarioLandCoverTransitions['Bare_Land to Croplands']) - parseFloat(scenarioLandCoverTransitions['Bare_Land to Artificial']);
+    var croplands = parseFloat(landCoverBaseData['Croplands']) + parseFloat(scenarioLandCoverTransitions['Bare_Land to Croplands']) + parseFloat(scenarioLandCoverTransitions['Grasslands to Croplands']) + parseFloat(scenarioLandCoverTransitions['Tree_Cover to Croplands']);
+    var grasslands = parseFloat(landCoverBaseData['Grasslands']) + parseFloat(scenarioLandCoverTransitions['Bare_Land to Grasslands']) + parseFloat(scenarioLandCoverTransitions['Tree_Cover to Grasslands']) - parseFloat(scenarioLandCoverTransitions['Grasslands to Artificial']) - parseFloat(scenarioLandCoverTransitions['Grasslands to Croplands']);
+    var treeCover = parseFloat(landCoverBaseData['Tree_Cover']) - parseFloat(scenarioLandCoverTransitions['Tree_Cover to Artificial']) - parseFloat(scenarioLandCoverTransitions['Tree_Cover to Croplands']) - parseFloat(scenarioLandCoverTransitions['Tree_Cover to Grasslands']);
+    var waterBodies = parseFloat(landCoverBaseData['Water_Bodies']);
+    var wetlands = parseFloat(landCoverBaseData['Wetlands']);
 
     var updatedLandCover = {
         Artificial: artificial,
