3f17dd70b5bce32f0ebfb00a3129fb1b99caf5cd
Simon Reynolds <reynolds.simon4@gmail.com>
2021-02-01 16:01:50 +0100

    save scenario test

--- ScenarioPlanner.js
+++ ScenarioPlanner.js
@@ -61,41 +61,41 @@ exports.saveScenario = function(regionalData, scenarioLandCoverTransitions, curr
     // currentRegion = currentRegion.set('landCover', updatedLandCoverData)
 
     // var currentRegionLandCover = landCoverBaseData.evaluate(function(data) {
-    var currentRegionLandCover = landCoverBaseData.evaluate(function(data) {
-        print(data)
-        print(scenarioLandCoverTransitions)
-        print(scenarioLandCoverTransitions['Tree_Cover to Artificial'])
+    // landCoverBaseData.evaluate(function(data) {
+    //     print(data)
+    //     print(scenarioLandCoverTransitions)
+    //     print(scenarioLandCoverTransitions['Tree_Cover to Artificial'])
 
-        var artificial = parseFloat(data['Artificial']) + parseFloat(scenarioLandCoverTransitions['Tree_Cover to Artificial']) + parseFloat(scenarioLandCoverTransitions['Grasslands to Artificial']) + parseFloat(scenarioLandCoverTransitions['Bare_Land to Artificial']);
-        var bareLand = parseFloat(data['Bare_Land']) - parseFloat(scenarioLandCoverTransitions['Bare_Land to Grasslands']) - parseFloat(scenarioLandCoverTransitions['Bare_Land to Croplands']) - parseFloat(scenarioLandCoverTransitions['Bare_Land to Artificial']);
-        var croplands = parseFloat(data['Croplands']) + parseFloat(scenarioLandCoverTransitions['Bare_Land to Croplands']) + parseFloat(scenarioLandCoverTransitions['Grasslands to Croplands']) + parseFloat(scenarioLandCoverTransitions['Tree_Cover to Croplands']);
-        var grasslands = parseFloat(data['Grasslands']) + parseFloat(scenarioLandCoverTransitions['Bare_Land to Grasslands']) + parseFloat(scenarioLandCoverTransitions['Tree_Cover to Grasslands']) - parseFloat(scenarioLandCoverTransitions['Grasslands to Artificial']) - parseFloat(scenarioLandCoverTransitions['Grasslands to Croplands']);
-        var treeCover = parseFloat(data['Tree_Cover']) - parseFloat(scenarioLandCoverTransitions['Tree_Cover to Artificial']) - parseFloat(scenarioLandCoverTransitions['Tree_Cover to Croplands']) - parseFloat(scenarioLandCoverTransitions['Tree_Cover to Grasslands']);
-        var waterBodies = parseFloat(data['Water_Bodies']);
-        var wetlands = parseFloat(data['Wetlands']);
+    //     var artificial = parseFloat(data['Artificial']) + parseFloat(scenarioLandCoverTransitions['Tree_Cover to Artificial']) + parseFloat(scenarioLandCoverTransitions['Grasslands to Artificial']) + parseFloat(scenarioLandCoverTransitions['Bare_Land to Artificial']);
+    //     var bareLand = parseFloat(data['Bare_Land']) - parseFloat(scenarioLandCoverTransitions['Bare_Land to Grasslands']) - parseFloat(scenarioLandCoverTransitions['Bare_Land to Croplands']) - parseFloat(scenarioLandCoverTransitions['Bare_Land to Artificial']);
+    //     var croplands = parseFloat(data['Croplands']) + parseFloat(scenarioLandCoverTransitions['Bare_Land to Croplands']) + parseFloat(scenarioLandCoverTransitions['Grasslands to Croplands']) + parseFloat(scenarioLandCoverTransitions['Tree_Cover to Croplands']);
+    //     var grasslands = parseFloat(data['Grasslands']) + parseFloat(scenarioLandCoverTransitions['Bare_Land to Grasslands']) + parseFloat(scenarioLandCoverTransitions['Tree_Cover to Grasslands']) - parseFloat(scenarioLandCoverTransitions['Grasslands to Artificial']) - parseFloat(scenarioLandCoverTransitions['Grasslands to Croplands']);
+    //     var treeCover = parseFloat(data['Tree_Cover']) - parseFloat(scenarioLandCoverTransitions['Tree_Cover to Artificial']) - parseFloat(scenarioLandCoverTransitions['Tree_Cover to Croplands']) - parseFloat(scenarioLandCoverTransitions['Tree_Cover to Grasslands']);
+    //     var waterBodies = parseFloat(data['Water_Bodies']);
+    //     var wetlands = parseFloat(data['Wetlands']);
 
-        var updatedLandCover = {
-            Artificial: artificial,
-            Bare_Land: bareLand,
-            Croplands: croplands,
-            Grasslands: grasslands,
-            Tree_Cover: treeCover,
-            Water_Bodies: waterBodies,
-            Wetlands: wetlands
-        }
+    //     var updatedLandCover = {
+    //         Artificial: artificial,
+    //         Bare_Land: bareLand,
+    //         Croplands: croplands,
+    //         Grasslands: grasslands,
+    //         Tree_Cover: treeCover,
+    //         Water_Bodies: waterBodies,
+    //         Wetlands: wetlands
+    //     }
 
-        print('Land Cover:', updatedLandCover)
+    //     print('Land Cover:', updatedLandCover)
 
-        var updatedLandCoverData = landCoverData.set(scenarioName, updatedLandCover);
-        currentRegion = currentRegion.set('landCover', updatedLandCoverData);
+    //     var updatedLandCoverData = landCoverData.set(scenarioName, updatedLandCover);
+    //     currentRegion = currentRegion.set('landCover', updatedLandCoverData);
 
-        // var currentRegionLandCover = currentRegion.set('landCover', updatedLandCoverData)
-        // return currentRegion.set('landCover', updatedLandCoverData)
+    //     // var currentRegionLandCover = currentRegion.set('landCover', updatedLandCoverData)
+    //     // return currentRegion.set('landCover', updatedLandCoverData)
 
-        // var updatedRegionalData = allRegionsExcludingCurrentRegion.merge(ee.FeatureCollection([currentRegion]))
+    //     // var updatedRegionalData = allRegionsExcludingCurrentRegion.merge(ee.FeatureCollection([currentRegion]))
 
-        // return updatedRegionalData
-    })
+    //     // return updatedRegionalData
+    // })
 
     var updatedRegionalData = allRegionsExcludingCurrentRegion.merge(ee.FeatureCollection([currentRegion]))
 
